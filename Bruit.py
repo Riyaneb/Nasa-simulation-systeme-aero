89f66a5182af5d02feecb7b8dde05079a88a6213
Riyaneb <riyane.bouakaz@etu.cyu.fr>
2026-04-19 16:56:58 +0200

    Maintenant les paramètres constants sont supprimés et les paramètres et capteurs sont normalisés

--- Bruit.py
+++ Bruit.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sqlite3
+from sklearn.preprocessing import StandardScaler
 
 DataBase = sqlite3.connect("Base_de_donnée_des_test.db")
 
@@ -7,9 +8,7 @@ df = pd.read_sql("SELECT * FROM Train_clean_FD001",DataBase)
 
 colonne_base = ["unit number","time, in cycles","file origin","RUL"]
 
-colonne_parametre = []
-for i in range(1,4):
-    colonne_parametre.append(f"operationnal setting {i}")
+colonne_parametre = list(df.filter(like='operationnal').columns)
 
 colonne_capteur = list(df.columns)
 
@@ -24,30 +23,33 @@ df = df.sort_values(["unit number","time, in cycles"],ascending=[True,True])
 
 moyenne_glissante = df.groupby('unit number')[colonne_capteur].rolling(window=15,min_periods=1).mean() #  pour chaque valeur d'un capteur associé à un moteur, je calcule ça moyenne à partir des 15 cycle d'avant et je le met dans la cellules i
 moyenne_glissante.reset_index(level=0,drop=True,inplace=True) #enleve le multiindex
+moyenne_glissante.rename(columns={element : element + " rm" for element in colonne_capteur},inplace=True)
 print(moyenne_glissante)
-df_moyenne_glissante = df.copy()
-df_moyenne_glissante[colonne_capteur] = moyenne_glissante
-df_moyenne_glissante.rename(columns={element : element + " rm" for element in colonne_capteur},inplace=True)
 print("\n\n")
-print(df_moyenne_glissante)
+
 
 #Ecart-type glissante :
 
 ecart_glissante = df.groupby('unit number')[colonne_capteur].rolling(window=15,min_periods=1).std(ddof=0)
 ecart_glissante.reset_index(level=0,drop=True,inplace=True)
-df_ecart_glissante = df.copy()
-df_ecart_glissante[colonne_capteur] = ecart_glissante
-df_ecart_glissante.rename(columns={element : element + " rs" for element in colonne_capteur},inplace=True)
+ecart_glissante.rename(columns={element : element + " rs" for element in colonne_capteur},inplace=True)
 print("\n\n")
-print(df_ecart_glissante) 
 
-df2 = pd.merge(df,df_moyenne_glissante,on=["unit number","time, in cycles"],how="inner") #on colle les df en faisant attention a ce qu'elles s'imbriquent bien
-df2 = pd.merge(df,df_ecart_glissante,on=["unit number","time, in cycles"],how="inner")
 
-print("\n,\n")
+df2 = pd.concat([df,moyenne_glissante,ecart_glissante],axis=1) #on colle les df
+
 print(df2.shape)
 
-# La c'est facultatif pour un random forest mais obligatoire pour d'autre algorithme comme regression linéaire
+# Là c'est facultatif pour un random forest mais obligatoire pour d'autre algorithme comme regression linéaire
 
+scale = StandardScaler()
+colonne_normaliser = list(df2.columns)
+for element in colonne_base:
+    colonne_normaliser.remove(element)
+
+df2[colonne_normaliser] = scale.fit_transform(df2[colonne_normaliser])
+print(df2)
+
+df2.to_sql(name="Train_Normalisé_FD001",con=DataBase,if_exists="replace",index=False)
 
 DataBase.close()
--- transformation.py
+++ transformation.py
@@ -5,6 +5,8 @@ DataBase = sqlite3.connect("Base_de_donnée_des_test.db")
 
 df = pd.read_sql("SELECT * FROM Train",DataBase)
 
+#Capteurs Morts :
+
 colonne_capteurs = [f"sensor measurement {i}" for i in range(1,22)] 
 variance = df.groupby("file origin")[colonne_capteurs].var() #on crée des sous groupes FD001-4 et on calcule la variances de chaque capteur dans chaque fichier
 
@@ -22,9 +24,19 @@ variance_capteur_utile_FD001 = variance_FD001.drop(index=liste_capteur_mort) #on
 print("\nCapteurs utiles du fichier FD001 : \n")
 print(variance_capteur_utile_FD001)
 
+#Paramètre inutile :
+
+colonne_parametre = []
+for i in range(1,4):
+    colonne_parametre.append(f"operationnal setting {i}")
+
+variance2 = df.groupby("file origin")[colonne_parametre].var()
+variance_2_FD001 = variance2.loc["FD001"]
+liste_parametre_mort = list(variance_2_FD001[variance_2_FD001 == 0].index)
 
 df_fd001 = df[df["file origin"] == "FD001"].copy() 
-df_fd001 = df_fd001.drop(columns=liste_capteur_mort) #on garde que les capteurs du fichier FD001 et on retire les capteurs morts
+df_fd001 = df_fd001.drop(columns=liste_capteur_mort) #on garde que les capteurs utiles du fichier FD001 et on retire les capteurs morts
+df_fd001 = df_fd001.drop(columns=liste_parametre_mort) #on garde que les paramètres utiles du fichier FD001 et on retire les paramètres constant
 print(df_fd001)
 df_fd001.to_sql(name="Train_clean_FD001",con=DataBase,if_exists="replace",index=False)
 
