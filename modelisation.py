fb1c0dd4e36724f6c25efce94d438a3b0aa34663
Riyaneb <riyane.bouakaz@etu.cyu.fr>
2026-04-22 20:40:55 +0200

    Ajout de graphique sur des moteurs spécifiques de validation

--- modelisation.py
+++ modelisation.py
@@ -4,6 +4,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 DataBase = sqlite3.connect("Base_de_donnée_des_test.db")
 df = pd.read_sql("SELECT * FROM Train_Normalisé_FD001",DataBase)
@@ -34,6 +35,7 @@ mae = mean_absolute_error(y_val, prediction_val_y)
 print(f"RMSE val : {rmse:.2f}")
 print(f"MAE val  : {mae:.2f}")
 
+plt.figure()
 plt.scatter(y_val, prediction_val_y, s=10)
 plt.plot([0, 125], [0, 125], 'r--', label="Prédiction parfaite")
 plt.xlabel("RUL réelle")
@@ -43,5 +45,28 @@ plt.legend()
 plt.grid(True, alpha=0.3)
 plt.savefig("images/regression_lineaire.png", bbox_inches="tight")
 plt.show()
+plt.close()
+
+MoteurHasard = random.choice(liste_moteur_val)
+df2 = pd.read_sql(f"""SELECT * FROM Train_Normalisé_FD001 WHERE "unit number"={MoteurHasard} """,DataBase)
+x_moteur_hasard = df2.drop(columns=suppr)
+y_moteur_hasard = df2["RUL"]
+prediction_moteur_hasard = modele.predict(x_moteur_hasard)
+prediction_moteur_hasard = np.clip(prediction_moteur_hasard, 0, 125)
+
+rmse2 = np.sqrt(mean_squared_error(y_moteur_hasard, prediction_moteur_hasard))
+mae2 = mean_absolute_error(y_moteur_hasard, prediction_moteur_hasard)
+
+plt.figure()
+plt.scatter(y_moteur_hasard, prediction_moteur_hasard, s=10)
+plt.plot([0, 125], [0, 125], 'r--', label="Prédiction parfaite")
+plt.xlabel("RUL réelle")
+plt.ylabel("RUL prédite")
+plt.title(f"Régression linéaire avec RMSE = {rmse2:.2f} et MAE = {mae2:.2f} du moteur {MoteurHasard}")
+plt.legend()
+plt.grid(True, alpha=0.3)
+plt.savefig(f"images/regression_lineaire_du_moteur_{MoteurHasard}.png", bbox_inches="tight")
+plt.show()
+plt.close()
 
 DataBase.close()
